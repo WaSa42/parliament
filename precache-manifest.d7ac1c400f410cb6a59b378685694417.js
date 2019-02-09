d9aa7859fb02e4afc5e6b56bf8d4c8a3542242d6
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2019-02-09 02:47:24 +0100

    Updates

---
+++ precache-manifest.d7ac1c400f410cb6a59b378685694417.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "50f7fd14ca63b8b05bdbf0554a5092d8",
+    "url": "/parliament/static/media/gb.50f7fd14.svg"
+  },
+  {
+    "revision": "4f1b7af5c0bae6aae85c3e7ba9401a85",
+    "url": "/parliament/static/media/gb.4f1b7af5.svg"
+  },
+  {
+    "revision": "f8952213641bba462c7314007909d394",
+    "url": "/parliament/static/media/fr.f8952213.svg"
+  },
+  {
+    "revision": "b1156355de9691d768df19a8a2b44da4",
+    "url": "/parliament/static/media/fr.b1156355.svg"
+  },
+  {
+    "revision": "f5f773ca34aebd6534c0",
+    "url": "/parliament/static/js/runtime~main.f5f773ca.js"
+  },
+  {
+    "revision": "9cbcc900f9df2170616a",
+    "url": "/parliament/static/js/main.9cbcc900.chunk.js"
+  },
+  {
+    "revision": "01353336c157a28a4e81",
+    "url": "/parliament/static/js/1.01353336.chunk.js"
+  },
+  {
+    "revision": "9cbcc900f9df2170616a",
+    "url": "/parliament/static/css/main.0b57c19d.chunk.css"
+  },
+  {
+    "revision": "01353336c157a28a4e81",
+    "url": "/parliament/static/css/1.64cac4ec.chunk.css"
+  },
+  {
+    "revision": "1f07cdb3d0e6cd8e4a57bb85d4ccc938",
+    "url": "/parliament/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/parliament/precache-manifest.d7ac1c400f410cb6a59b378685694417.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/parliament/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
